4d156e407a2d5206539d48ec6a6123fc0de2d739
Elie Gavoty <eliegavoty@free.fr>
2019-10-16 21:42:54 +0200

    ansible_renne

---
+++ provisionner/terraform/main.tf
@@ -0,0 +1,78 @@
+variable "do_api_token" {}
+variable "do_sshkey_id" {}
+
+provider "digitalocean" {
+    token = "${var.do_api_token}"
+}
+
+locals {
+  haproxy_balancer_count = 1
+  app_node_count = 2
+  awx_node_count = 1
+}
+
+resource "digitalocean_droplet" "balancers" {
+  count = "${local.haproxy_balancer_count}"
+  name = "balancer${count.index}"
+  image = "centos-7-x64"
+  size = "1gb"
+  region = "ams3"
+  ssh_keys = ["${var.do_sshkey_id}"]
+}
+
+resource "digitalocean_droplet" "appservers" {
+  count = "${local.app_node_count}"
+  name = "appserver${count.index}"
+  image = "ubuntu-18-04-x64"
+  size = "1gb"
+  region = "ams3"
+  ssh_keys = ["${var.do_sshkey_id}"]
+}
+
+resource "digitalocean_droplet" "awxnodes" {
+  count = "${local.awx_node_count}"
+  name = "awx${count.index}"
+  image = "centos-7-x64"
+  size = "4gb"
+  region = "ams3"
+  ssh_keys = ["${var.do_sshkey_id}"]
+}
+
+
+## Ansible mirroring hosts section
+# Using https://github.com/nbering/terraform-provider-ansible/ to be installed manually (third party provider)
+# Copy binary to ~/.terraform.d/plugins/
+
+resource "ansible_host" "ansible_balancers" {
+  count = "${local.haproxy_balancer_count}"
+  inventory_hostname = "appserver${count.index}"
+  groups = ["balancers"]
+  vars = {
+    ansible_host = "${element(digitalocean_droplet.balancers.*.ipv4_address, count.index)}"
+  }
+}
+
+resource "ansible_host" "ansible_appservers" {
+  count = "${local.app_node_count}"
+  inventory_hostname = "balancer${count.index}"
+  groups = ["appservers"]
+  vars = {
+    ansible_host = "${element(digitalocean_droplet.appservers.*.ipv4_address, count.index)}"
+  }
+}
+
+resource "ansible_host" "ansible_awxnodes" {
+  count = "${local.awx_node_count}"
+  inventory_hostname = "awx${count.index}"
+  groups = ["awxnodes"]
+  vars = {
+    ansible_host = "${element(digitalocean_droplet.awxnodes.*.ipv4_address, count.index)}"
+  }
+}
+
+resource "ansible_group" "all" {
+  inventory_group_name = "all"
+  vars = {
+    # ansible_python_interpreter = "/usr/bin/python3"
+  }
+}
---
+++ provisionner/terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+
+
+do_api_token = "756b5fc0c8dfc1ca86bb001f8bcd3c35d236f0422380507ecaac55e58de6ad53"
+do_sshkey_id = "41:d9:ad:ba:e0:32:73:58:4f:09:28:15:f2:1d:ae:4f"
